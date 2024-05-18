93fd932d5521cfc64465ba3656450464f48df393
Muhammad Ibrahim <160228904+Ibrahimnazee@users.noreply.github.com>
2024-05-18 14:36:46 +0500

    Add files via upload
     this countdown timer ensures accurate timing down to the second. With seamless user interaction and real-time updates, you can trust it to keep you on track and focused.

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.yellow('*'.repeat(45)))
+ console.log(chalk.bold.italic.blue("\n \t WELCOME TO COUNTDOWN TIMER\n"))
+ console.log(chalk.yellow('*'.repeat(45)))
+
+  const response = await inquirer.prompt({
+    name: "number",
+    type: "input",
+    message: chalk.bold.italic("Please Enter The Amount Of Seconds"),
+    validate: (input) => {
+      const num = Number(input);
+      if (isNaN(num)) {
+        return chalk.bold.red("Please enter a valid number");
+      } else if (num >= 60) {
+        return chalk.bold.red("Seconds must be less than 60");
+      } else {
+        return true;
+      }
+    },
+  });
+
+  let input = Number(response.number);
+
+  function startTime(val: number) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const timeDiff = differenceInSeconds(intervalTime, currentTime);
+      if (timeDiff <= 0) {
+        console.log(chalk.bold.italic.red("Time Has Expired"));
+        
+        process.exit(0);
+      }
+
+      let min = Math.floor((timeDiff % (3600 * 24)) / 3600); 
+      let sec = Math.floor(timeDiff % 60);
+      console.log(
+        chalk.bold.italic.green(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`)
+      );
+    }, 1000);
+  }
+
+  startTime(input);
+
+
+
+
